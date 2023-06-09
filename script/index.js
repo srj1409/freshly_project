9bd86e7693b1bcbdf24c5c586b47a8adde241404
Savali Jadhav <102408177+srj1409@users.noreply.github.com>
2023-06-09 21:40:00 +0530

    project initiated

---
+++ script/index.js
@@ -0,0 +1,9 @@
+document.querySelector(".signup").addEventListener("click",function(){
+    window.location.href="signup.html"
+});
+
+document.querySelector(".login").addEventListener("click",function(){
+    window.location.href="login.html"
+});
+
+ 
